c2172f6fced1031962fde7108393c85047ce4ff6
Ryan Tse <admin@iwizardpro.com>
2016-04-03 18:37:58 -0700

    Move election application about dialog to separate class.

--- ElectionUI.py
+++ ElectionUI.py
@@ -9,6 +9,7 @@ from Election import Election
 from ElectionUIRacePanel import ElectionUIRacePanel
 from ElectionRace import ElectionRace
 from ElectionRaceRound import ElectionRaceRound
+from ElectionApplicationAbout import ElectionApplicationAbout
 
 
 class ElectionUI(wx.Frame):
@@ -202,15 +203,7 @@ class ElectionUI(wx.Frame):
         self.Layout()
 
     def show_about(self, event):
-        about_info = wx.adv.AboutDialogInfo()
-        about_info.SetName("UCSB AS Election Tabulator")
-        about_info.SetVersion("1.0")
-        about_info.SetDescription("Displays and tabulates voter ballots utilizing a single transferrable vote system\nwith support for custom election configurations and dynamic ballot format parsing.")
-        about_info.SetCopyright("Copyright (C) 2016 The Regents of the University of California.")
-        about_info.SetWebSite("https://www.as.ucsb.edu/")
-        about_info.AddDeveloper("Ryan Tse <tse@umail.ucsb.edu>")
-
-        wx.adv.AboutBox(about_info, self)
+        ElectionApplicationAbout(self)
 
     def ui_combobox_event(self, event):
         if event.GetEventObject() is self.combo_box_race:
