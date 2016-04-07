f6e369ad14e5f8e4819aeb92fa4ae9c50ddf2cc8
Ryan Tse <admin@iwizardpro.com>
2016-04-07 11:52:27 -0700

    Add election configuration for UCSB AS 2016 election.

---
+++ data-2016/parser/ElectionBallotParser.py
@@ -0,0 +1,112 @@
+###############
+# Ballot Parser for Qualtrics
+#
+# This ballot parser has been tailored to the ballot
+# system used by UCSB. If you use another software
+# to define ballots, ensure the data returned by the
+# ballot parser returns data in the following fashion:
+#
+# [
+#    {
+#        "ballot_id": "unique_ballot_id",
+#        "ballot_data": {
+#            "race_id": [
+#                "candidate_id",
+#                "candidate_id",
+#                ...
+#            ],
+#            "race_id": [
+#                "candidate_id",
+#                "candidate_id",
+#                ...
+#           ],
+#           ...
+#        }
+#    },
+#    {
+#        "ballot_id": "unique_ballot_id",
+#        "ballot_data": {
+#            "race_id": [
+#                "candidate_id",
+#                "candidate_id",
+#                ...
+#            ],
+#            "race_id": [
+#                "candidate_id",
+#                "candidate_id",
+#                ...
+#           ],
+#           ...
+#        }
+#    },
+#    ...
+# ]
+#
+# The race_id value should correspond to the value
+# specified in the configuration file.
+#
+# Each list identified by the race_id should be in
+# voting-choice order, where the first candidate
+# within the list corresponds to the ballot's first
+# choice vote.
+#
+# The candidate_id should correspond to the value
+# returned by the election candidate parser.
+#
+# Last Modified: February 24, 2016
+###############
+import csv
+
+
+def parse(ballot_file_path, races):
+    ballots_data = []
+
+    # Open the ballot file.
+    with open(ballot_file_path, encoding="UTF-8", errors="ignore") as ballot_file:
+        ballot_file_csv = csv.reader(ballot_file)
+        ballot_file_data = list(ballot_file_csv)
+        ballot_columns = {}
+
+        parser_groups = {}
+        for race in races:
+            parser_groups[race.extended_data()["parser_group"]] = race
+            ballot_columns[race] = []
+
+        for column_index in range(len(ballot_file_data[0])):
+            if ballot_file_data[0][column_index] in parser_groups:
+                ballot_columns[parser_groups[ballot_file_data[0][column_index]]].append(column_index)
+
+        for row in range(2, len(ballot_file_data)):
+            # Process each individual voter.
+            ballot_data = {}
+
+            # Loop through each race and get preferences.
+            ballot_race_data = {}
+            for race in races:
+                race_preferences = [None] * (len(race.candidates()) + 1)
+                for column in ballot_columns[race]:
+                    try:
+                        if ballot_file_data[1][column].strip() == "Write-In":
+                            race_order = int(ballot_file_data[row][column])
+                            if ballot_file_data[row][column + 1].strip():
+                                race_preferences[race_order] = race.get_candidate(ballot_file_data[row][column + 1].strip()).id()
+                        else:
+                            race_order = int(ballot_file_data[row][column])
+                            race_preferences[race_order] = race.get_candidate(ballot_file_data[1][column].strip()).id()
+                    except ValueError:
+                        pass
+                # Remove zeroth index (None) since candidates are ordered from 1 to N.
+                race_preferences.pop(0)
+                try:
+                    preference_max = race_preferences.index(None)
+                    race_preferences = race_preferences[0:preference_max]
+                except ValueError:
+                    pass
+                ballot_race_data[race.id()] = race_preferences
+
+            ballot_data["ballot_id"] = ballot_file_data[row][0]
+            ballot_data["ballot_data"] = ballot_race_data
+
+            ballots_data.append(ballot_data)
+
+    return ballots_data
---
+++ data-2016/parser/ElectionCandidateParser.py
@@ -0,0 +1,98 @@
+###############
+# Candidate Parser for Qualtrics
+#
+# This candidate parser has been tailored to the ballot
+# system used by UCSB. If you use another software
+# to define ballots, ensure the data returned by the
+# candidate parser returns data in the following fashion:
+#
+# {
+#   "race_id": [
+#       {
+#           "candidate_id": "unique_candidate_id",
+#           "candidate_name": "candidate_full_name",
+#           "candidate_party": "candidate_party"
+#       },
+#       {
+#           "candidate_id": "unique_candidate_id",
+#           "candidate_name": "candidate_full_name",
+#           "candidate_party": "candidate_party"
+#       },
+#       ...
+#   ],
+#   "race_id": [
+#       {
+#           "candidate_id": "unique_candidate_id",
+#           "candidate_name": "candidate_full_name",
+#           "candidate_party": "candidate_party"
+#       },
+#       {
+#           "candidate_id": "unique_candidate_id",
+#           "candidate_name": "candidate_full_name",
+#           "candidate_party": "candidate_party"
+#       },
+#       ...
+#   ],
+#   ...
+# }
+#
+# The race_id value should correspond to the value
+# specified in the configuration file.
+#
+# Each list identified by the race_id should contain
+# candidates in each race.
+#
+# The candidate_id value should be unique to
+# each candidate in each race. The candidate_id should
+# NOT be repeated in another race.
+#
+# Last Modified: February 24, 2016
+###############
+import csv
+
+
+def parse(candidate_file_path, races):
+    candidates_data = {}
+
+    # Open the ballot file.
+    with open(candidate_file_path, encoding="UTF-8", errors="ignore") as candidate_file:
+        candidate_file_csv = csv.reader(candidate_file)
+        candidate_file_data = list(candidate_file_csv)
+        race_columns = {}
+
+        parser_groups = {}
+        for race in races:
+            parser_groups[race.extended_data()["parser_group"]] = race
+            race_columns[race] = []
+
+        for column_index in range(len(candidate_file_data[0])):
+            if candidate_file_data[0][column_index] in parser_groups:
+                race_columns[parser_groups[candidate_file_data[0][column_index]]].append(column_index)
+
+        for race in races:
+            candidates = []
+            candidates_write_ins = []
+            for column_index in race_columns[race]:
+                if candidate_file_data[1][column_index].startswith("Write-In"):
+                    # Process write in candidates.
+                    if candidate_file_data[1][column_index].endswith("TEXT"):
+                        for row in range(2, len(candidate_file_data)):
+                            candidate_id = candidate_file_data[row][column_index].strip()
+                            if candidate_id and candidate_id not in candidates_write_ins:
+                                candidates.append({
+                                    "candidate_id": candidate_id,
+                                    "candidate_name": candidate_id,
+                                    "candidate_party": "Independent"
+                                })
+                                candidates_write_ins.append(candidate_id)
+                    continue
+
+                (candidate_name, candidate_party) = candidate_file_data[1][column_index].rsplit("-", 1)
+                candidates.append({
+                    "candidate_id": candidate_file_data[1][column_index],
+                    "candidate_name": candidate_name.strip(),
+                    "candidate_party": candidate_party.strip()
+                })
+            candidates_data[race.id()] = candidates
+
+    return candidates_data
